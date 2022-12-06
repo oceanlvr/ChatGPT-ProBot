a844de1982c63f7f05042da5c9ab6bab3ad23970
oceanlvr <657531018@qq.com>
2022-12-06 14:31:30 +0800

    fix vercel

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,13 +1,11 @@
 const { createNodeMiddleware, createProbot } = require("probot");
-const dotenv = require("dotenv")
 const app = require("../../../dist/index.js");
-const { APP_ID, WEBHOOK_SECRET, PRIVATE_KEY } = dotenv.config({ path: '../../../.env' }).parsed
 module.exports = createNodeMiddleware(app, {
   probot: createProbot({
     defaults: {
-      appId: APP_ID,
-      privateKey: PRIVATE_KEY,
-      secret: WEBHOOK_SECRET,
+      appId: process.env.APP_ID,
+      privateKey: process.env.PRIVATE_KEY,
+      secret: process.env.WEBHOOK_SECRET,
     }
   }),
   webhooksPath: "/api/github/webhooks",
