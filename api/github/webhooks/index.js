31207c6a38e6b8e8166c52f8c29560a819ca51da
oceanlvr <657531018@qq.com>
2022-12-06 17:21:06 +0800

    fix reply rebot

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,13 +1,6 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("../../../dist/index.js");
 module.exports = createNodeMiddleware(app, {
-  probot: createProbot({
-    defaults: {
-      appId: process.env.APP_ID,
-      privateKey: process.env.PRIVATE_KEY,
-      secret: process.env.WEBHOOK_SECRET,
-      githubToken: process.env.GITHUB_TOKEN
-    }
-  }),
+  probot: createProbot(),
   webhooksPath: "/api/github/webhooks",
 });
