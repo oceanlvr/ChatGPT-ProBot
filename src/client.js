232c9480ab8741394d276858f1b67a22442868a3
oceanlvr <657531018@qq.com>
2022-12-07 02:18:42 +0800

    fix ensureAuth bug

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 const { ChatGPTAPI } = require('@oceanlvr/chatgpt')
 
+const client = new ChatGPTAPI({ sessionToken: process.env.SESSION_TOKEN })
 module.exports = async function search(searchPrompt) {
-  const client = new ChatGPTAPI({ sessionToken: process.env.SESSION_TOKEN })
-  await client.ensureAuth()
+  // await client.ensureAuth()
   const reponse = await client.sendMessage(searchPrompt)
   return reponse
 }
