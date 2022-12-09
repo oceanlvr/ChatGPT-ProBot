1371c95c8d1aaf5354af95ac7240dab3b87ac94c
oceanlvr <657531018@qq.com>
2022-12-09 14:31:47 +0800

    fix json import error

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import json from "@rollup/plugin-json";
 
 export default {
   input: 'index.js',
@@ -9,11 +10,9 @@ export default {
     format: 'cjs',
   },
   plugins: [
-    resolve({ preferBuiltins: false, }),
+    json(),
+    nodeResolve({ preferBuiltins: true }),
     commonjs(),
     terser()
   ],
-  external: [
-    "tr46"
-  ]
 }
